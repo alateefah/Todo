dcc9e44837c445d728aa00625e5e699a63207056
Lateefah Abdulkareem <lateefah.abdulkareem@interswitchgroup.com>
2019-12-13 16:39:25 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const TodoApp = () => {
+
+    const [todo, setTodo] = useState({});
+    const [todos, pushTodo] = useState([])
+   
+    return (
+        <div className="App">
+      	    <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />            
+
+                <div className="input-group mb-3">
+                    <input type="text" className="form-control" placeholder="To do" onChange={e => setTodo(e.target.value)} name="todo" />
+                    <div className="input-group-append">
+                        <button className="btn btn-primary" type="button" onClick={() => pushTodo([...todos, todo])}>Add</button>
+                    </div>
+                </div>
+
+                <ul className="list-display">
+                    {todos.map((todo, i) => {
+                        return <li key={i}>{todo}</li>
+                    })}
+                </ul>
+            </header>
+        </div>
+    );
 }
 
-export default App;
+export default TodoApp;
