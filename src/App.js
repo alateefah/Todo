3ad4f856ece74e019a948f78e3d80d75743da07a
Lateefah Abdulkareem <lateefah.abdulkareem@interswitchgroup.com>
2019-12-14 17:57:07 +0100

    remove to do item

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ import './App.css';
 const TodoApp = () => {
 
     const [todo, setTodo] = useState({});
-    const [todos, pushTodo] = useState([])
+    const [todos, pushTodo] = useState([]);
+
+    function add () {
+        pushTodo([...todos, todo]);
+    }
+
+    function remove (item) {
+        pushTodo(todos.filter(todo => todo !== item));
+    }
    
     return (
         <div className="App">
@@ -15,13 +23,18 @@ const TodoApp = () => {
                 <div className="input-group mb-3">
                     <input type="text" className="form-control" placeholder="To do" onChange={e => setTodo(e.target.value)} name="todo" />
                     <div className="input-group-append">
-                        <button className="btn btn-primary" type="button" onClick={() => pushTodo([...todos, todo])}>Add</button>
+                        <button className="btn btn-primary" type="button" onClick={add}>Add</button>
                     </div>
                 </div>
 
                 <ul className="list-display">
                     {todos.map((todo, i) => {
-                        return <li key={i}>{todo}</li>
+                        return <li key={i}>
+                                    {todo} &nbsp;&nbsp;&nbsp;
+                                    <button type="button" className="close" aria-label="Close" onClick={()=>remove(todo)}>
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </li>
                     })}
                 </ul>
             </header>
